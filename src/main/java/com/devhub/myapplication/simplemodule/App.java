cc584fff4aab2ac9706de9f44f75ff8081bbedaf
kdaud <dkakumirizii@gmail.com>
2021-01-27 10:37:45 +0300

    microforntend api

---
+++ src/main/java/com/devhub/myapplication/simplemodule/App.java
@@ -0,0 +1,21 @@
+package com.devhub.myapplication.simplemodule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	static {
+		System.out.println("My Programming Journey is interesting and fun");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	static {
+		System.out.println("Academic ");
+	}
+}
---
+++ src/main/java/com/devhub/myapplication/simplemodule/api/Microfrontend.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.devhub.myapplication.simplemodule.api;
+
+
+/**
+ * @author opensource
+ *
+ */
+public class Microfrontend {
+	
+	static {
+		System.out.println("MF Squard is led by Porma Grace");
+	}
+	
+}
